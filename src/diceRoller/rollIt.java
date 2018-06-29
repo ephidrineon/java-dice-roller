882c3c057ed52dfa6a67b1672bace1848011bc0d
ephidrineon <40677366+ephidrineon@users.noreply.github.com>
2018-06-29 09:57:06 +0200

    DnD dice roller, written in Java
    Restructure of the program. Made it more organized and legible.
    
    You can now press q to exit the program. Program now won't quit after rolling a dice.

--- src/diceRoller/rollIt.java
+++ src/diceRoller/rollIt.java
@@ -4,44 +4,51 @@ import java.util.Scanner;
 public class rollIt {
 
     public static void main(String[] args) {
+        RollAction();
+    }
+
+
+    public static boolean ValidInput(int dVal, int sVal) {
+        if(dVal <= 100 && dVal >= 1 && sVal <= 100 && sVal >= 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void RollAction() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the dice you want to roll: ");
         String diceInput = scanner.next();
 
-        String diceInputElements[] = diceInput.split("d");
+        if(diceInput.equals("q") | diceInput.equals("Q")) {
+            System.exit(0);
+        }
 
+        String diceInputElements[] = diceInput.split("d");
 
         int numberOfDice = Integer.parseInt(diceInputElements[0]);
         int diceSides = Integer.parseInt(diceInputElements[1]);
         double diceResult = 0;
 
-        if(numberOfDice <= 100) {
-            if(numberOfDice >= 1) {
-                if(diceSides <= 100) {
-                    if(diceSides >= 2) {
-                        String bonusOutput = "";
-                        int i = 0;
-                        while(i < numberOfDice) {
-                            diceResult = Math.random() * diceSides;
-                            i++;
-                            bonusOutput += Math.round(diceResult) + " ";
-                        }
-
-                        long diceOutput = Math.round(diceResult);
-
-                        System.out.println(diceInput + " = " + diceOutput + ": " + bonusOutput);
-                    } else {
-                        System.out.println("You might want to check your inputs. You can't have more than 100 or less than 1 dice, and you can't have more than 100 or less than 2 sides to each dice.");
-                    }
-                    } else {
-                    System.out.println("You might want to check your inputs. You can't have more than 100 or less than 1 dice, and you can't have more than 100 or less than 2 sides to each dice.");
-                }
-                } else {
-                System.out.println("You might want to check your inputs. You can't have more than 100 or less than 1 dice, and you can't have more than 100 or less than 2 sides to each dice.");
+        if(ValidInput(numberOfDice, diceSides) == true) {
+            String bonusOutput = "";
+            int i = 0;
+            while (i < numberOfDice) {
+                diceResult = Math.random() * diceSides;
+                i++;
+                bonusOutput += Math.round(diceResult) + " ";
             }
-            } else {
-            System.out.println("You might want to check your inputs. You can't have more than 100 or less than 1 dice, and you can't have more than 100 or less than 2 sides to each dice.");
+
+            long diceOutput = Math.round(diceResult);
+
+            System.out.println(diceInput + " = " + diceOutput + ": " + bonusOutput);
+            RollAction();
+        } else {
+            System.out.println("Sorry, invalid input.");
+            RollAction();
         }
+
     }
 }
 
